4cd4d4b7271652c4337916c10dcf2a8ef277a564
chasehoff <crhoffman@student.fullsail.edu>
2020-02-26 20:32:25 -0500

    Fixed top Nav bar

--- app/components/nav/TopNav.js
+++ app/components/nav/TopNav.js
@@ -5,7 +5,7 @@ import { MdDashboard, MdSettings } from "react-icons/md";
 const TopNav = ({settingsToggle, dashboardToggle}) =>(
     <div style={styles.container}>
         <button onClick={dashboardToggle} style={styles.btn}>
-            <MdDashboard style={{ fontSize: '3em', margin: 'auto 0', color: 'lightgray' }} />
+            <MdDashboard style={{ display: "none", fontSize: '3em', margin: 'auto 0', color: 'lightgray' }} />
         </button>
         <div style={styles.imageWrapper}>
             <SVGLogo style={styles.logo} height="60px" />
@@ -28,6 +28,8 @@ const styles = {
         boxShadow: '0px 3px 13px -2px rgba(0,0,0,0.75)'
     },
     btn: {
-        backgroundColor: 'transparent'
+        display: 'flex',
+        backgroundColor: 'transparent',
+        alignSelf: 'flex-end'
     }
 }
